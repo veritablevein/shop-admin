9a7d5093aaf57ff515724b1a014affa801b246fd
veritablevein <veritablevein@gmail.com>
2022-05-17 17:03:23 +0800

    chore: 配置commitlint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    eslintPlugin({
+      // 配置选项
+    })
+  ]
 })
