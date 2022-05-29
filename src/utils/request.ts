69868081b9543409d61c4b050531e9c410aa59f9
veritablevein <veritablevein@gmail.com>
2022-05-30 01:01:10 +0800

    feat: 统一处理登录过期

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,6 +1,7 @@
+import router from '@/router/'
 import { store } from '@/store'
 import axios, { AxiosRequestConfig } from 'axios'
-import { ElMessage } from 'element-plus'
+import { ElMessage, ElMessageBox } from 'element-plus'
 
 const request = axios.create({
   // baseURL: import.meta.env.VITE_API_BASEURL
@@ -19,15 +20,57 @@ request.interceptors.request.use(function (config) {
   return Promise.reject(error)
 })
 
+// 控制登录过期的锁
+let isRefreshing = false
+
 // 响应拦截器
 request.interceptors.response.use(function (response) {
-  // 统一处理接口响应错误，比如 token过期、服务端异常等
-  if (response.data.status && response.data.status !== 200) {
-    ElMessage.error(response.data.msg || '请求失败，请稍后重试')
-    // 手动返回一个Promise异常
-    return Promise.reject(response.data)
+  const status = response.data.status
+
+  // 正确的情况
+  if (!status || status === 200) {
+    return response
   }
-  return response
+
+  // 错误的情况
+
+  // 统一处理登录过期
+  if (status === 410000) {
+    if (isRefreshing) { return Promise.reject(response) }
+    isRefreshing = true
+    ElMessageBox.confirm('您的登录已过期，您可以取消停留在此页面，或确认重新登录', '登录过期', {
+      confirmButtonClass: '确认',
+      cancelButtonClass: '取消'
+    }).then(() => {
+      // 清除本地过期的登录状态
+      store.commit('setUser', null)
+      // 跳转到登录页面
+      router.push({
+        name: 'login',
+        query: {
+          redirect: router.currentRoute.value.fullPath
+        }
+      })
+      // 抛出异常
+    }).finally(() => {
+      isRefreshing = false
+    })
+
+    // 在内部消化掉这个业务异常
+    return Promise.reject(response)
+  }
+
+  ElMessage.error(response.data.msg || '请求失败，请稍后重试')
+  // 手动返回一个Promise异常
+  return Promise.reject(response)
+
+  // 统一处理接口响应错误，比如 token过期、服务端异常等
+  // if (response.data.status && response.data.status !== 200) {
+  //   ElMessage.error(response.data.msg || '请求失败，请稍后重试')
+  //   // 手动返回一个Promise异常
+  //   return Promise.reject(response.data)
+  // }
+  // return response
 }, function (error) {
   // Any status codes that falls outside the range of 2xx cause this function to trigger
   // Do something with response error
